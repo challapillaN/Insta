91828ceb01d227aee772e4bcd206c336d2bc9dfc
challapillaN <ratnamchellapilla@gmail.com>
2022-07-07 15:28:45 +0530

    first commit

---
+++ src/test/java/com/neha/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
